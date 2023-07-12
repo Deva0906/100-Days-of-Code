98cccd446736238ff70a25f69a51ea2785386953
Deva0906 <138346027+Deva0906@users.noreply.github.com>
2023-07-12 09:42:41 +0530

    Update Day1.java

--- SEMCOURSES.java
+++ SEMCOURSES.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 class Day1
 {
-	public static void main (String[] args) throws java.lang.Exception
+	public static void main (String[] args)
 
 	{
 	  Scanner sc=new Scanner (System.in);
