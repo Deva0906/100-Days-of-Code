c94808b314d7426f11e4577d354f40bcf1346a44
Deva0906 <138346027+Deva0906@users.noreply.github.com>
2023-07-12 09:49:21 +0530

    Update Day3.java

--- LTIME.java
+++ LTIME.java
@@ -2,21 +2,21 @@ import java.util.Scanner;
 
 class Day3
 {
-	public static void main (String[] args) throws java.lang.Exception
-	{
+	public static void main (String[] args) 
+{
 		Scanner sc = new Scanner(System.in);
 		int n =sc.nextInt();
 		for (int i=0;i<n;i++)
 		{
 		  int X = sc.nextInt();
 		  if(1<=X && 4>=X)
-{
-System.out.println("Yes");
-}
-		  else 
-{
-System.out.println("No");
-}
+		  {
+			  System.out.println("Yes");
+		  }
+		  else
+		  {
+			  System.out.println("No");
+		  }
 		  
 		} 
 	}
