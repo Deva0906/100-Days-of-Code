634e943828e594a23122fc6b2e7f302345d2fc82
Deva0906 <138346027+Deva0906@users.noreply.github.com>
2023-07-12 09:46:34 +0530

    Update Day2.java

--- WATERREQ.java
+++ WATERREQ.java
@@ -2,15 +2,15 @@ import java.util.Scanner;
 
 class Day2
 {
-	public static void main (String[] args) throws java.lang.Exception
+	public static void main (String[] args) 
 	{
-		Scanner sc= new Scanner(System.in);    
+        Scanner sc= new Scanner(System.in);    
         int T= sc.nextInt();  
         
         for(int i=0; i<T;i++)
-{
-            int N= sc.nextInt();
- System.out.println(N*2);
+	{
+	int N= sc.nextInt();
+	System.out.println(N*2);
     	}
 	}
 }
