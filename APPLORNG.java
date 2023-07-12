aa5db7966f91afccf9cc1bed6ba1f8250dc1e7b2
Deva0906 <138346027+Deva0906@users.noreply.github.com>
2023-07-12 09:52:29 +0530

    Update Day4.java

--- APPLORNG.java
+++ APPLORNG.java
@@ -2,18 +2,18 @@ import java.util.Scanner;
 
 class Day4
 {
-	public static void main (String[] args) throws java.lang.Exception
+	public static void main (String[] args) 
 	{
 	    Scanner sc=new Scanner(System.in);
 	    int X=sc.nextInt();
 	    int A=sc.nextInt();
 	    int B=sc.nextInt();if((A+B)>X)
-{
-System.out.println("No");
-}
-else
-{
-System.out.println("Yes");
-}
-}
+	    {
+		    System.out.println("No");
+	    }
+	    else
+	    {
+		    System.out.println("Yes");
+	    }
+	}
 }
