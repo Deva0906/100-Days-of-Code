fc3e8ad63fae0319b22155289167fc02ea3bec4e
Deva0906 <138346027+Deva0906@users.noreply.github.com>
2023-07-15 12:37:30 +0530

    Update Day4.java

--- FBC.java
+++ FBC.java
@@ -6,17 +6,18 @@ public static void main (String[] args)
 {
 Scanner sc=new Scanner(System.in);
 int T=sc.nextInt();
-		for(int i=0;i<T;i++)
-{
-int K=sc.nextInt();
-		    int X=sc.nextInt();
-		    if(K>=X){
-		        System.out.println(K-X);
-}
-else
-{
-System.out.println(X-K);
-}
-}
+	for(int i=0;i<T;i++)
+	{
+		int K=sc.nextInt();
+		int X=sc.nextInt();
+		if(K>=X)
+		{
+			System.out.println(K-X);
+		}
+		else
+		{
+			System.out.println(X-K);
+		}
+	}
 }
 }
