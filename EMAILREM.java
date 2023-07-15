c1f078e2e8be39d1f18b0b605665dce047beadaf
Deva0906 <138346027+Deva0906@users.noreply.github.com>
2023-07-15 12:38:53 +0530

    Update Day3.java

--- EMAILREM.java
+++ EMAILREM.java
@@ -3,10 +3,10 @@ import java.util.Scanner;
 class Day3
 {
 public static void main (String[] args)
-{
-Scanner sc=new Scanner(System.in);
-int U=sc.nextInt();
-int N=sc.nextInt();
-System.out.println(U-N);
-}
+  {
+    Scanner sc=new Scanner(System.in);
+    int U=sc.nextInt();
+    int N=sc.nextInt();
+    System.out.println(U-N);
+  }
 }
