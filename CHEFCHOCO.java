4aae3eb460c8dce19640793a03d02e5dcb12b26a
Deva0906 <138346027+Deva0906@users.noreply.github.com>
2023-07-15 13:21:49 +0530

    Update Day9.java

--- CHEFCHOCO.java
+++ CHEFCHOCO.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 class Day9 
 {
-    public static void main(String[] args)
+    public static void main(String[] args) 
     {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
