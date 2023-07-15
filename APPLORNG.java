8b799ea481cd38b2f8932ab000d0e857da6f16af
Deva0906 <138346027+Deva0906@users.noreply.github.com>
2023-07-15 12:31:38 +0530

    Update APPLORNG.java

--- APPLORNG.java
+++ APPLORNG.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 class Day4
 {
-	public static void main (String[] args) 
+	public static void main (String[] args)
 	{
 	    Scanner sc=new Scanner(System.in);
 	    int X=sc.nextInt();
