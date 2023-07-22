649348180b47530c0682a4ad62a8a66bb472310b
Deva0906 <138346027+Deva0906@users.noreply.github.com>
2023-07-22 15:35:38 +0530

    Update Day11.java

--- NOTEBOOK.java
+++ NOTEBOOK.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 class Day11
 {
     public static void main(String[] args) 
-{
+    {
         Scanner sc=new Scanner(System.in);
         int T=sc.nextInt();
         for(int i=0;i<T;i++)
