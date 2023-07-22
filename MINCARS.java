aea189f7801e9330fd03c11b2dfa8d0db929e6ca
Deva0906 <138346027+Deva0906@users.noreply.github.com>
2023-07-22 15:40:00 +0530

    Update Day15.java

--- MINCARS.java
+++ MINCARS.java
@@ -1,7 +1,8 @@
 import java.util.Scanner;
 
 class Day15 {
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         Scanner sc=new Scanner(System.in);
         int T=sc.nextInt();
         for(int i=0;i<T;i++)
