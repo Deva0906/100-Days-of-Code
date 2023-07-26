2642bf5a3996178259aba3d85e9f9ccf5548d615
Deva0906 <138346027+Deva0906@users.noreply.github.com>
2023-07-26 08:32:34 +0530

    Update Day24.java

--- SUBSCRIBE.java
+++ SUBSCRIBE.java
@@ -2,7 +2,8 @@ import java.util.Scanner;
 
 class Day24
 {
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         Scanner sc=new Scanner(System.in);
         int T=sc.nextInt();
         for(int i=0;i<T;i++)
